32a29dc5ccde5d96f25351f3585e4229efe733ad
Matheo Daninos <matheo.daninos@gmail.com>
2022-01-07 17:54:46 +0100

    refactoring

--- main.go
+++ main.go
@@ -1,62 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	"github.com/webmamba/my-crypto/blockchain"
 	"fmt"
 )
 
-type BlockChain struct {
-	Blocks []*Block
-}
-
-type Block struct {
-	Data []byte
-	PrevHash []byte
-	Hash []byte
-}
-
-func (block *Block) GenerateHash() {
-	blockContent := bytes.Join([][]byte{block.PrevHash, block.Data}, []byte{})
-
-	hash := sha256.Sum256(blockContent)
-
-	block.Hash = hash[:]
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	block := chain.CreateBlock([]byte(data))
-
-	chain.Blocks = append(chain.Blocks, block)
-}
-
-func (chain *BlockChain) CreateBlock(data []byte) (block *Block){
-	lastBlock := chain.GetLastBlock()
-
-	block = &Block{Data: data, PrevHash: lastBlock.Hash}
-	block.GenerateHash();
-
-	return block
-}
-
-func (chain *BlockChain) GetLastBlock() (block *Block) {
-	return chain.Blocks[len(chain.Blocks) - 1]
-}
-
-func Genesis() (block *Block) {
-	firstBlock := Block{Data: []byte("First"), PrevHash: []byte{}}
-	firstBlock.GenerateHash()
-
-	return &firstBlock
-}
-
-func InitBlockChain() (blockChain *BlockChain) {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
-
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("Second")
 	chain.AddBlock("Third")
