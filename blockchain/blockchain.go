13b9efc20d6a3d961e5cad50569eada296201bce
Matheo Daninos <matheo.daninos@gmail.com>
2022-01-08 16:02:44 +0100

    add proof of work

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,7 +3,13 @@ package blockchain
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
+	"log"
+	"math/big"
+	"time"
 )
+
+const difficulty = 12
 
 type BlockChain struct {
 	Blocks []*Block
@@ -13,14 +19,16 @@ type Block struct {
 	Data []byte
 	PrevHash []byte
 	Hash []byte
+	Time []byte
+	Nonce int64
 }
 
-func (block *Block) GenerateHash() {
-	blockContent := bytes.Join([][]byte{block.PrevHash, block.Data}, []byte{})
+func GenerateHash(block Block, nonce []byte) (hash [32]byte) {
+	blockContent := bytes.Join([][]byte{block.PrevHash, block.Data, block.Time, nonce}, []byte{})
 
-	hash := sha256.Sum256(blockContent)
+	hash = sha256.Sum256(blockContent)
 
-	block.Hash = hash[:]
+	return hash
 }
 
 func (chain *BlockChain) AddBlock(data string) {
@@ -29,13 +37,57 @@ func (chain *BlockChain) AddBlock(data string) {
 	chain.Blocks = append(chain.Blocks, block)
 }
 
-func (chain *BlockChain) CreateBlock(data []byte) (block *Block){
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
+}
+
+func (chain *BlockChain) CreateBlock(data []byte) (block *Block) {
+	var intHash big.Int
+	var currentHash [32]byte
+
 	lastBlock := chain.GetLastBlock()
 
-	block = &Block{Data: data, PrevHash: lastBlock.Hash}
-	block.GenerateHash();
+	block = &Block{Data: data, PrevHash: lastBlock.Hash, Time: []byte(time.Now().String())}
+
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256 - difficulty))
+
+	nonce := int64(0)
+
+	for {
+		currentHash = GenerateHash(*block, ToHex(nonce))
+		intHash.SetBytes(currentHash[:])
+
+		if intHash.Cmp(target) == -1 {
+			break
+		}
+
+		nonce++
+	}
+
+	block.Hash = currentHash[:]
+	block.Nonce = nonce
 
 	return block
+}
+
+func (block *Block) Validate() bool {
+	var intHash big.Int
+
+	nonce := block.Nonce
+	hash := GenerateHash(*block, ToHex(nonce))
+
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256 - difficulty))
+
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(target) == -1
 }
 
 func (chain *BlockChain) GetLastBlock() (block *Block) {
@@ -44,7 +96,7 @@ func (chain *BlockChain) GetLastBlock() (block *Block) {
 
 func Genesis() (block *Block) {
 	firstBlock := Block{Data: []byte("First"), PrevHash: []byte{}}
-	firstBlock.GenerateHash()
+	firstBlock.Hash = []byte("hashhhh")
 
 	return &firstBlock
 }
--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
 		fmt.Printf("Block number: %d\n", i)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Timestamp: %s\n", block.Time)
+		fmt.Printf("Validation: %t\n", block.Validate())
 		fmt.Printf("----------------\n")
 	}
 }
